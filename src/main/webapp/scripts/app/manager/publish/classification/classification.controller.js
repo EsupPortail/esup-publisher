10b879212f8978332d438bd4e19da955b2721ca6
GIP RECIA - Julien Gribonvald <julien.gribonvald@gmail.com>
2016-05-12 16:14:05 +0200

    Automtically select first Classification when only one is available

--- src/main/webapp/scripts/app/manager/publish/classification/classification.controller.js
+++ src/main/webapp/scripts/app/manager/publish/classification/classification.controller.js
@@ -7,33 +7,21 @@ angular.module('publisherApp')
 
         $scope.classificationsList = [];
 
-        //$scope.selectedClassifications = [];
-
-        /*if ($scope.$parent.classifications.length > 0) {
-            $scope.selectedClassifications = angular.copy($scope.$parent.classifications);
-            console.log("selected calssif :", JSON.stringify($scope.selectedClassifications) );
-        }*/
-
         if (angular.equals([],$scope.$parent.classifications) && loadedClassifications) {
             $scope.$parent.classifications = angular.copy(loadedClassifications);
-            //console.log('loaded classifications :', $scope.$parent.classifications);
         }
 
         //TODO managing level of publication = 2 in "grouping" Feeds in categories name
 
         $scope.loadAll = function() {
             Classification.query({publisherId: $scope.$parent.publisher.id, isPublishing: true}, function(result) {
-                //$scope.classificationsList = result;
                 angular.forEach(result, function(obj) {
-                    /*var ticked = false;
-                    if (containsSelectedClassification(obj.contextKey)) {
-                        ticked = true;
-                    }*/
                     $scope.classificationsList.push({contextKey : obj.contextKey, icon : obj.iconUrl, name: obj.name, description : obj.description, color : obj.color});
-                    //$scope.classificationsList.push({contextKey : obj.contextKey, icon : obj.iconUrl, name: obj.name, description : obj.description, ticked: ticked});
                 });
-                //console.log("after load already selected : ",$scope.$parent.classifications );
-                //console.log("after load",$scope.classificationsList );
+                // if only one Classification is enable automatic select it !
+                if ($scope.classificationsList.length == 1) {
+                    $scope.toggleSelection($scope.classificationsList[0].contextKey);
+                }
             });
         };
 
@@ -43,24 +31,6 @@ angular.module('publisherApp')
         };
 
         $scope.loadAll();
-
-        /*$scope.localLang = {
-            selectAll       : $filter('translate')("global.form.multiSelect.selectAll"),
-            selectNone      : $filter('translate')("global.form.multiSelect.selectNone"),
-            reset           : $filter('translate')("global.form.multiSelect.reset"),
-            search          : $filter('translate')("global.form.multiSelect.search"),
-            nothingSelected : $filter('translate')("global.form.multiSelect.nothingSelected")
-        };*/
-
-        /*$scope.validateClassifications = function() {
-            console.log("validate Classifications : ");
-            $scope.$parent.classifications = [];
-            angular.forEach($scope.selectedClassifications, function(obj) {
-                //$scope.$parent.classifications.push(obj.contextKey);
-                $scope.$parent.classifications.push(obj);
-            });
-            console.log("Select Classifications are : ", JSON.stringify($scope.$parent.classifications));
-        };*/
 
         $scope.toggleSelection = function (contextKey) {
             var i = 0, idx=-1;
@@ -94,14 +64,5 @@ angular.module('publisherApp')
             }
             return false;
         };
-
-        /*$scope.getBackgroundStyle = function(classification){
-            if (classification.icon) {
-                var style = "'background-image':'url(" + classification.icon + ")'";
-                console.log("with background ", style);
-                return style;
-            }
-            return "";
-        };*/
 
     });
