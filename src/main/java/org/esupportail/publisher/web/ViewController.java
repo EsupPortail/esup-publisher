7ceac52af062b5df9af07ff2116b415ea19b7b77
GIP RECIA - Julien Gribonvald <julien.gribonvald@gmail.com>
2017-02-27 13:53:55 +0100

    bis - Set Item enclosure url and contents with media url relative to context app

--- src/main/java/org/esupportail/publisher/web/ViewController.java
+++ src/main/java/org/esupportail/publisher/web/ViewController.java
@@ -10,6 +10,10 @@ import javax.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.esupportail.publisher.Application;
 import org.esupportail.publisher.domain.AbstractItem;
+import org.esupportail.publisher.domain.Flash;
+import org.esupportail.publisher.domain.Media;
+import org.esupportail.publisher.domain.News;
+import org.esupportail.publisher.domain.Resource;
 import org.esupportail.publisher.domain.enums.ItemStatus;
 import org.esupportail.publisher.repository.ItemRepository;
 import org.esupportail.publisher.repository.predicates.ItemPredicates;
@@ -36,17 +40,28 @@ public class ViewController {
     private ServiceUrlHelper urlHelper;
 
     @RequestMapping(value = "/item/{item_id}", produces = MediaType.TEXT_HTML_VALUE )
-    public String itemView(Map<String, Object> model, @PathVariable("item_id") Long itemId, HttpServletRequest request){
+    public String itemView(Map<String, Object> model, @PathVariable("item_id") Long itemId, HttpServletRequest request) {
+        log.debug("Request to render in viewer, item with id {}", itemId);
         if (itemId == null) return "itemError";
         final AbstractItem item = itemRepository.findOne(ItemPredicates.ItemWithStatus(itemId, ItemStatus.PUBLISHED));
-        log.debug("item found {}", item);
+        log.debug("Item found {}", item);
         if (item == null) return "itemNotFound";
-        if (item.getEnclosure() != null) {
-            if (item.getEnclosure().matches("^https?://.*$")) {
-                item.setEnclosure(item.getEnclosure());
-            } else {
-                item.setEnclosure(urlHelper.getRootAppUrl(request) + item.getEnclosure());
-            }
+
+        final String baseUrl = !request.getContextPath().isEmpty() ? request.getContextPath() + "/" : "/";
+
+        // all items has an enclosure but optional
+        item.setEnclosure(replaceRelativeUrl(item.getEnclosure(), baseUrl));
+
+        // looking to replace img src with path of object with body attribute of for specific property
+        if (item instanceof News) {
+            ((News) item).setBody(replaceBodyUrl(((News) item).getBody(), baseUrl));
+        } else if (item instanceof Flash) {
+            ((Flash) item).setBody(replaceBodyUrl(((Flash) item).getBody(), baseUrl));
+        } else if (item instanceof Resource) {
+            ((Resource)item).setRessourceUrl(replaceRelativeUrl(((Resource)item).getRessourceUrl(), baseUrl));
+        } else if (!(item instanceof Media)) {
+            log.error("Warning a new type of Item wasn't managed at this place, the item is :", item);
+            throw new IllegalStateException("Warning missing type management of :" + item.toString());
         }
         model.put("item", item);
         return "item";
@@ -58,5 +73,19 @@ public class ViewController {
         properties.load(Application.class.getResourceAsStream("/version.properties"));
         final String version = properties.getProperty("version");
         return version;
+    }
+
+    private String replaceBodyUrl(final String body, final String baseUrl){
+        if (body != null && !body.trim().isEmpty()){
+            return body.replaceAll("src=\"files/", "src=\"" + baseUrl + "files/");
+        }
+        return body;
+    }
+
+    private String replaceRelativeUrl(final String localUrl, final String baseUrl){
+        if (localUrl != null && !localUrl.trim().isEmpty() && !localUrl.matches("^https?://.*$")){
+            return baseUrl + localUrl;
+        }
+        return localUrl;
     }
 }
