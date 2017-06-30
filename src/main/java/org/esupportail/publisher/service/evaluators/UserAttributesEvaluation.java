e42cf889be685b6a2d7c895c8ee7ddd3c7800adf
GIP RECIA - Julien Gribonvald <julien.gribonvald@gmail.com>
2017-06-30 10:28:29 +0200

    License format

--- src/main/java/org/esupportail/publisher/service/evaluators/UserAttributesEvaluation.java
+++ src/main/java/org/esupportail/publisher/service/evaluators/UserAttributesEvaluation.java
@@ -1,12 +1,11 @@
 /**
  * Copyright (C) 2014 Esup Portail http://www.esup-portail.org
- * Copyright (C) 2014 RECIA http://www.recia.fr
  * @Author (C) 2012 Julien Gribonvald <julien.gribonvald@recia.fr>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 				http://www.apache.org/licenses/LICENSE-2.0
+ *                 http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
