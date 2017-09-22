04edf502ae4763195311c69ed5821faf2768476e
GIP RECIA - Julien Gribonvald <julien.gribonvald@gmail.com>
2017-09-22 16:40:11 +0200

    Solving problems on cookies path
    - update tomcat version
    - provide configuration of tomcat to force context options MapperContextRootRedirectEnabled = true and SessionCookiePathUsesTrailingSlash = true, depending on tomcat version theses attributes have differents values
    - for cookie CSRF-TOKEN having a trailingslish on context path to avoid problems on firefox who needs it (having sometimes 2 cookies on same name but path different with or without /
    - maybe need to check on angular-tranlate cookies but not sure as it's only needed on front side (expect LocalResolver on spring), but don't see any problems on !

---
+++ src/main/java/org/esupportail/publisher/config/TomcatConfig.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright (C) 2014 Esup Portail http://www.esup-portail.org
+ * @Author (C) 2012 Julien Gribonvald <julien.gribonvald@recia.fr>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *                 http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.esupportail.publisher.config;
+
+import java.util.Arrays;
+
+import org.apache.catalina.Context;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatContextCustomizer;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by jgribonvald on 25/01/16.
+ */
+@Configuration
+public class TomcatConfig {
+
+    @Bean
+    public EmbeddedServletContainerFactory servletContainer() {
+        TomcatEmbeddedServletContainerFactory factory = new TomcatEmbeddedServletContainerFactory();
+        factory.setTomcatContextCustomizers(Arrays.asList(new CustomCustomizer()));
+        return factory;
+    }
+
+    public static class CustomCustomizer implements TomcatContextCustomizer {
+        public void customize(Context context) {
+            context.setMapperContextRootRedirectEnabled(true);
+            context.setSessionCookiePathUsesTrailingSlash(true);
+        }
+    }
+}
