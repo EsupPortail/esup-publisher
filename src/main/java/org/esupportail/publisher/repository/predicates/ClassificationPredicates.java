d25edd58a9210c3850403a4cef96235072e2ab84
GIP RECIA - Julien Gribonvald <julien.gribonvald@gmail.com>
2020-02-14 16:08:52 +0100

    fix: Classification Order Predicate on wrong order

--- src/main/java/org/esupportail/publisher/repository/predicates/ClassificationPredicates.java
+++ src/main/java/org/esupportail/publisher/repository/predicates/ClassificationPredicates.java
@@ -36,44 +36,38 @@ import com.querydsl.core.types.Predicate;
  */
 public final class ClassificationPredicates {
 
-	public static Predicate ClassificationsOfOrganization(Long organizationId) {
-		final QAbstractClassification classif = QAbstractClassification.abstractClassification;
-		return classif.publisher.context.organization.id.eq(organizationId);
-	}
+    public static Predicate ClassificationsOfOrganization(Long organizationId) {
+        final QAbstractClassification classif = QAbstractClassification.abstractClassification;
+        return classif.publisher.context.organization.id.eq(organizationId);
+    }
 
     public static Predicate notNull() {
-		return QAbstractClassification.abstractClassification.isNotNull();
-	}
+        return QAbstractClassification.abstractClassification.isNotNull();
+    }
 
-	public static OrderSpecifier<?> classifOrderByDisplayOrderType(DisplayOrderType displayOrder) {
-		final QAbstractClassification classif = QAbstractClassification.abstractClassification;
-		switch (displayOrder) {
-		case LAST_CREATED_MODIFIED_FIRST:
-			return classif.lastModifiedDate.coalesce(classif.createdDate).asc();
-		case ONLY_LAST_CREATED_FIRST:
-			return classif.createdDate.asc();
-		case NAME:
-			return classif.name.asc();
-		case START_DATE:
-			return classif.createdDate.asc();
-		case CUSTOM:
-			return classif.displayOrder.asc();
-		default:
-			return classif.name.asc();
-		}
-	}
+    public static OrderSpecifier<?> classifOrderByDisplayOrderType(DisplayOrderType displayOrder) {
+        final QAbstractClassification classif = QAbstractClassification.abstractClassification;
+        switch (displayOrder) {
+            case LAST_CREATED_MODIFIED_FIRST:
+                return classif.lastModifiedDate.coalesce(classif.createdDate).desc();
+            case ONLY_LAST_CREATED_FIRST:
+            case START_DATE: //no start date is available on classif, so use the created date.
+                return classif.createdDate.desc();
+            case CUSTOM:
+                return classif.displayOrder.asc();
+            default:
+                return classif.name.asc();
+        }
+    }
 
     public static OrderSpecifier<?> categoryOrderByDisplayOrderType(DisplayOrderType displayOrder) {
         final QCategory classif = QCategory.category;
         switch (displayOrder) {
             case LAST_CREATED_MODIFIED_FIRST:
-                return classif.lastModifiedDate.coalesce(classif.createdDate).asc();
+                return classif.lastModifiedDate.coalesce(classif.createdDate).desc();
             case ONLY_LAST_CREATED_FIRST:
-                return classif.createdDate.asc();
-            case NAME:
-                return classif.name.asc();
-            case START_DATE:
-                return classif.createdDate.asc();
+            case START_DATE: //no start date is available on classif, so use the created date.
+                return classif.createdDate.desc();
             case CUSTOM:
                 return classif.displayOrder.asc();
             default:
@@ -85,13 +79,10 @@ public final class ClassificationPredicates {
         final QAbstractFeed classif = QAbstractFeed.abstractFeed;
         switch (displayOrder) {
             case LAST_CREATED_MODIFIED_FIRST:
-                return classif.lastModifiedDate.coalesce(classif.createdDate).asc();
+                return classif.lastModifiedDate.coalesce(classif.createdDate).desc();
             case ONLY_LAST_CREATED_FIRST:
-                return classif.createdDate.asc();
-            case NAME:
-                return classif.name.asc();
-            case START_DATE:
-                return classif.createdDate.asc();
+            case START_DATE: //no start date is available on classif, so use the created date.
+                return classif.createdDate.desc();
             case CUSTOM:
                 return classif.displayOrder.asc();
             default:
@@ -99,10 +90,10 @@ public final class ClassificationPredicates {
         }
     }
 
-	public static Predicate CategoryClassification() {
-		final QAbstractClassification classif = QAbstractClassification.abstractClassification;
-		return classif.instanceOf(Category.class);
-	}
+    public static Predicate CategoryClassification() {
+        final QAbstractClassification classif = QAbstractClassification.abstractClassification;
+        return classif.instanceOf(Category.class);
+    }
 
     public static Predicate CategoryClassificationOfPublisher(long pubId) {
         final QAbstractClassification classif = QAbstractClassification.abstractClassification;
@@ -114,39 +105,39 @@ public final class ClassificationPredicates {
         return classif.publisher.id.eq(pubId);
     }
 
-	public static Predicate CategoryOfPublisher(long pubId) {
-		final QCategory category = QCategory.category;
-		return category.publisher.id.eq(pubId);
-	}
+    public static Predicate CategoryOfPublisher(long pubId) {
+        final QCategory category = QCategory.category;
+        return category.publisher.id.eq(pubId);
+    }
 
-	public static Predicate AbstractFeedsOfCategory(Category cat) {
-		final QAbstractFeed classif = QAbstractFeed.abstractFeed;
-		return classif.parent.id.eq(cat.getId());
-	}
+    public static Predicate AbstractFeedsOfCategory(Category cat) {
+        final QAbstractFeed classif = QAbstractFeed.abstractFeed;
+        return classif.parent.id.eq(cat.getId());
+    }
     public static Predicate AbstractFeedsOfCategory(long catId) {
         final QAbstractFeed classif = QAbstractFeed.abstractFeed;
         return classif.parent.id.eq(catId);
     }
 
-	public static Predicate InternalFeedClassification() {
-		final QAbstractClassification classif = QAbstractClassification.abstractClassification;
-		return classif.instanceOf(InternalFeed.class);
-	}
+    public static Predicate InternalFeedClassification() {
+        final QAbstractClassification classif = QAbstractClassification.abstractClassification;
+        return classif.instanceOf(InternalFeed.class);
+    }
 
-	public static Predicate InternalFeedClassificationOfPublisher(Publisher pub) {
-		final QAbstractClassification classif = QAbstractClassification.abstractClassification;
-		return classif.publisher.id.eq(pub.getId()).and(InternalFeedClassification());
-	}
+    public static Predicate InternalFeedClassificationOfPublisher(Publisher pub) {
+        final QAbstractClassification classif = QAbstractClassification.abstractClassification;
+        return classif.publisher.id.eq(pub.getId()).and(InternalFeedClassification());
+    }
 
-	public static Predicate ExternalFeedClassification() {
-		final QAbstractClassification classif = QAbstractClassification.abstractClassification;
-		return classif.instanceOf(ExternalFeed.class);
-	}
+    public static Predicate ExternalFeedClassification() {
+        final QAbstractClassification classif = QAbstractClassification.abstractClassification;
+        return classif.instanceOf(ExternalFeed.class);
+    }
 
-	public static Predicate ExternalFeedClassificationOfPublisher(Publisher pub) {
-		final QAbstractClassification classif = QAbstractClassification.abstractClassification;
-		return classif.publisher.id.eq(pub.getId()).and(ExternalFeedClassification());
-	}
+    public static Predicate ExternalFeedClassificationOfPublisher(Publisher pub) {
+        final QAbstractClassification classif = QAbstractClassification.abstractClassification;
+        return classif.publisher.id.eq(pub.getId()).and(ExternalFeedClassification());
+    }
 
     public static Predicate AbstractFeedClassification() {
         final QAbstractClassification classif = QAbstractClassification.abstractClassification;
