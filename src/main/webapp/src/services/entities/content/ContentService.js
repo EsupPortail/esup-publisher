8d68d6c13b1b8abf88820e715c04d6d05aa9479b
Sébastien Lafon <sebastien.lafon@live.fr>
2022-02-09 15:23:49 +0100

    Suppression de la baseUrl dans les liens du body des contenus

--- src/main/webapp/src/services/entities/content/ContentService.js
+++ src/main/webapp/src/services/entities/content/ContentService.js
@@ -9,6 +9,7 @@ class ContentService {
   get (id) {
     return FetchWrapper.getJson('api/contents/' + id).then(response => {
       if (response.data) {
+        addBaseUrlToBodyLinks(response.data)
         response.data.item.endDate = DateUtils.convertLocalDateFromServer(response.data.item.endDate)
         response.data.item.startDate = DateUtils.convertLocalDateFromServer(response.data.item.startDate)
         response.data.item.validatedDate = DateUtils.convertDateTimeFromServer(response.data.item.validatedDate)
@@ -21,6 +22,7 @@ class ContentService {
 
   update (content) {
     const copy = Object.assign({}, content)
+    removeBaseUrlToBodyLinks(content)
     copy.item.startDate = DateUtils.convertLocalDateToServer(copy.item.startDate)
     copy.item.endDate = DateUtils.convertLocalDateToServer(copy.item.endDate)
     return FetchWrapper.putJson('api/contents', copy)
@@ -28,6 +30,7 @@ class ContentService {
 
   save (content) {
     const copy = Object.assign({}, content)
+    removeBaseUrlToBodyLinks(content)
     copy.item.startDate = DateUtils.convertLocalDateToServer(copy.item.startDate)
     copy.item.endDate = DateUtils.convertLocalDateToServer(copy.item.endDate)
     return FetchWrapper.postJson('api/contents', copy)
@@ -35,6 +38,36 @@ class ContentService {
 
   delete (id) {
     return FetchWrapper.deleteJson('api/contents/' + id)
+  }
+}
+
+/**
+ * Méthode permettant de rajouter dans les liens du body d'un contenu la baseUrl.
+ *
+ * @param {Objecct} content Objet Contenu avec le body à mettre à jour
+ */
+function addBaseUrlToBodyLinks (content) {
+  if (content.item && content.item.body && content.linkedFiles && content.linkedFiles.length > 0) {
+    content.linkedFiles.forEach(link => {
+      if (content.item.body.includes('"' + link.uri + '"')) {
+        content.item.body = content.item.body.replaceAll('"' + link.uri + '"', '"' + process.env.VUE_APP_BACK_BASE_URL + link.uri + '"')
+      }
+    })
+  }
+}
+
+/**
+ * Méthode permettant de supprimer dans les liens du body d'un contenu la baseUrl.
+ *
+ * @param {Objecct} content Objet Contenu avec le body à mettre à jour
+ */
+function removeBaseUrlToBodyLinks (content) {
+  if (content.item && content.item.body && content.linkedFiles && content.linkedFiles.length > 0) {
+    content.linkedFiles.forEach(link => {
+      if (content.item.body.includes('"' + process.env.VUE_APP_BACK_BASE_URL + link.uri + '"')) {
+        content.item.body = content.item.body.replaceAll('"' + process.env.VUE_APP_BACK_BASE_URL + link.uri + '"', '"' + link.uri + '"')
+      }
+    })
   }
 }
 
---
+++ src/main/webapp/tests/unit/contentService.spec.js
@@ -0,0 +1,126 @@
+
+import ContentService from '@/services/entities/content/ContentService.js'
+import FetchWrapper from '@/services/util/FetchWrapper.js'
+
+jest.mock('@/services/util/FetchWrapper.js')
+
+describe('ContentService.js tests', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
+  it('test 1 ContentService - query', (done) => {
+    const response = {
+      data: [],
+      headers: []
+    }
+    FetchWrapper.getJson = jest.fn().mockReturnValue(Promise.resolve(response))
+
+    ContentService.query().then(value => {
+      expect(FetchWrapper.getJson).toHaveBeenCalledTimes(1)
+      expect(FetchWrapper.getJson).toHaveBeenCalledWith('api/contents')
+      expect(value).toStrictEqual(response)
+      done()
+    })
+  })
+
+  it('test 2 ContentService - get', (done) => {
+    const response = {
+      data: {
+        item: {
+          endDate: '2021-12-01',
+          startDate: '2021-12-02',
+          validatedDate: '2021-12-03',
+          createdDate: '2021-12-04',
+          lastModifiedDate: '2021-12-05',
+          body: '<a href="link/to/file">test</a>'
+        },
+        linkedFiles: [
+          {
+            uri: 'link/to/file'
+          },
+          {
+            uri: 'link/to/file2'
+          }
+        ]
+      },
+      headers: []
+    }
+    FetchWrapper.getJson = jest.fn().mockReturnValue(Promise.resolve(response))
+
+    const id = 1
+    ContentService.get(id).then(value => {
+      expect(FetchWrapper.getJson).toHaveBeenCalledTimes(1)
+      expect(FetchWrapper.getJson).toHaveBeenCalledWith('api/contents/' + id)
+      expect(toString.call(value.data.item.endDate)).toStrictEqual('[object Date]')
+      expect(toString.call(value.data.item.startDate)).toStrictEqual('[object Date]')
+      expect(toString.call(value.data.item.validatedDate)).toStrictEqual('[object Date]')
+      expect(toString.call(value.data.item.createdDate)).toStrictEqual('[object Date]')
+      expect(toString.call(value.data.item.lastModifiedDate)).toStrictEqual('[object Date]')
+      expect(value.data.item.body).toStrictEqual('<a href="' + process.env.VUE_APP_BACK_BASE_URL + 'link/to/file">test</a>')
+      done()
+    })
+  })
+
+  it('test 3 ContentService - update', (done) => {
+    FetchWrapper.putJson = jest.fn().mockReturnValue(Promise.resolve({}))
+
+    const data = {
+      item: {
+        endDate: new Date('2021', '11', '01'),
+        startDate: new Date('2021', '11', '02'),
+        body: '<a href="' + process.env.VUE_APP_BACK_BASE_URL + 'link/to/file">test</a>'
+      },
+      linkedFiles: [
+        {
+          uri: 'link/to/file'
+        },
+        {
+          uri: 'link/to/file2'
+        }
+      ]
+    }
+    ContentService.update(data).then(() => {
+      expect(FetchWrapper.putJson).toHaveBeenCalledTimes(1)
+      expect(FetchWrapper.putJson).toHaveBeenCalledWith('api/contents', expect.objectContaining({
+        item: expect.objectContaining({
+          endDate: '2021-12-01',
+          startDate: '2021-12-02',
+          body: '<a href="link/to/file">test</a>'
+        })
+      }))
+      done()
+    })
+  })
+
+  it('test 4 ContentService - save', (done) => {
+    FetchWrapper.postJson = jest.fn().mockReturnValue(Promise.resolve({}))
+
+    const data = {
+      item: {
+        endDate: new Date('2021', '11', '01'),
+        startDate: new Date('2021', '11', '02'),
+        body: '<a href="' + process.env.VUE_APP_BACK_BASE_URL + 'link/to/file">test</a>'
+      },
+      linkedFiles: [
+        {
+          uri: 'link/to/file'
+        },
+        {
+          uri: 'link/to/file2'
+        }
+      ]
+    }
+    ContentService.save(data).then(() => {
+      expect(FetchWrapper.postJson).toHaveBeenCalledTimes(1)
+      expect(FetchWrapper.postJson).toHaveBeenCalledWith('api/contents', expect.objectContaining({
+        item: expect.objectContaining({
+          endDate: '2021-12-01',
+          startDate: '2021-12-02',
+          body: '<a href="link/to/file">test</a>'
+        })
+      }))
+      done()
+    })
+  })
+})
