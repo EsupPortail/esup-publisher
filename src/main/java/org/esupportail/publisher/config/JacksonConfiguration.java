2443bb92a2a99a53ae8d65b6fbcd66e8a25ec2ac
GRIBONVALD Julien <julien.gribonvald@recia.fr>
2022-03-30 12:54:11 +0200

    fix: ProblemModule package change

--- src/main/java/org/esupportail/publisher/config/JacksonConfiguration.java
+++ src/main/java/org/esupportail/publisher/config/JacksonConfiguration.java
@@ -21,7 +21,7 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.module.afterburner.AfterburnerModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.zalando.problem.ProblemModule;
+import org.zalando.problem.jackson.ProblemModule;
 import org.zalando.problem.violations.ConstraintViolationProblemModule;
 
 @Configuration
