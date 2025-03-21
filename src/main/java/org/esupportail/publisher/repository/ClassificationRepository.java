326a1e4e0c714d919c458375008d7f478b01a678
Julien Gribonvald <julien.gribonvald@recia.fr>
2025-03-21 17:18:06 +0100

    refactor: remove unuser method on repositories

--- src/main/java/org/esupportail/publisher/repository/ClassificationRepository.java
+++ src/main/java/org/esupportail/publisher/repository/ClassificationRepository.java
@@ -18,17 +18,12 @@
  */
 package org.esupportail.publisher.repository;
 
-import java.util.Set;
-
 import org.esupportail.publisher.domain.AbstractClassification;
-import org.esupportail.publisher.domain.Publisher;
 
 /**
  * @author GIP RECIA - Julien Gribonvald 24 juil. 2014
  */
 public interface ClassificationRepository<T extends AbstractClassification>
 		extends AbstractRepository<T, Long> {
-
-	Set<T> findByPublisher(Publisher publisher);
 
 }
